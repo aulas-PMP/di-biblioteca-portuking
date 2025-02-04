b1126ef3733b1ead3adef3ba3f7886804beb1a57
Pablo Dopazo Suarez <dopazopablo2004@gmail.com>
2025-02-04 18:56:09 +0100

    Diseño básico da interfaz

--- src/reproductor/Controller.java
+++ src/reproductor/Controller.java
@@ -10,6 +10,5 @@ public class Controller {
     public void initialize() {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
     }
 }
